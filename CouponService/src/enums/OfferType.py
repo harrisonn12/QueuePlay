519e346dfc27068974ccdfa7626e702278a7d2ba
Harrison Nguyen <harrisonnguyen77@gmail.com>
2025-03-16 09:58:56 -0700

    fix: remove duplicate class

--- CouponService/src/enums/OfferType.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class OfferType(Enum):
-    BOGO = 'bogo'
-    DISCOUNT = 'discount'
-    FREE = 'free'
--- CouponService/src/models/Offer.py
+++
@@ -1,8 +0,0 @@
-from CouponService.src.enums.OfferType import OfferType
-
-class Offer:
-    offerType: OfferType
-    value: str
-    count: int
-    product_id: int
-    expirationDate: str
