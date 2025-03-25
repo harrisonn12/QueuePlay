01a53310abd1ece8f3584db7d9d5a8b6562ba1d8
Daniel <dandcalvo@gmail.com>
2025-03-25 16:42:03 -0700

    chore: supabase configuration

--- backend/routers/PaymentDatabaseRouter.py
+++ backend/routers/PaymentDatabaseRouter.py
@@ -3,8 +3,13 @@ from PaymentService.adapters.DatabaseAdapter import DatabaseAdapter
 
 router = APIRouter(
     prefix="/paymentDatabase",
-    tags=["Payment Service: Database Adapter"],
+    tags=["Payment Service: Supabase"],
+    
     responses={404: {"description": "Not found"}}
 )
 
 databaseAdapter = DatabaseAdapter()
+
+@router.get('/')
+def firstFetch():
+    return databaseAdapter.firstFetch()
