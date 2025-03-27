b46c2ede62a7310cb2d1d077894f19c7cb7173c9
Daniel <dandcalvo@gmail.com>
2025-03-27 10:07:24 -0700

    fix: rebase supabase-integration branch with main

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import argparse
-from CouponService.src.databases.CouponsDatabase import CouponsDatabase
-=======
-import argparse, uvicorn
-from commons.adapters.ChatGptAdapter import ChatGptAdapter
-from configuration.AppConfig import AppConfig
-from configuration.AppConfig import Stage
-from dotenv import load_dotenv
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
->>>>>>> c0b4eea (fix: create routers for FastAPI to clean up main file)
-from LobbyService.LobbyService import LobbyService
-from LobbyService.src.QRCodeGenerator import QRCodeGenerator
-from QuestionService.QuestionService import QuestionService
-from QuestionService.src.QuestionAnswerSetGenerator import QuestionAnswerSetGenerator
-<<<<<<< HEAD
-from commons.adapters.ChatGptAdapter import ChatGptAdapter
-from configuration.AppConfig import AppConfig
-from configuration.AppConfig import Stage
 from CouponService.CouponService import CouponService
 from CouponService.src.CouponIdGenerator import CouponIdGenerator
 from CouponService.src.OfferSelectionProcessor import OfferSelectionProcessor
@@ -25,27 +5,29 @@ from CouponService.src.adapters.AvailableOffersAdapter import AvailableOffersAda
 from CouponService.src.adapters.CouponRedemptionAdapter import CouponRedemptionAdapter
 from CouponService.src.databases import AssignedCouponsDatabase
 from CouponService.src.databases.AssignedCouponsDatabase import AssignedCouponsDatabase
+from CouponService.src.databases.CouponsDatabase import CouponsDatabase
 from CouponService.src.models.CustomerMessagingProcessor import CustomerMessagingProcessor
+from LobbyService.LobbyService import LobbyService
+from LobbyService.src.QRCodeGenerator import QRCodeGenerator
+from QuestionService.QuestionService import QuestionService
+from QuestionService.src.QuestionAnswerSetGenerator import QuestionAnswerSetGenerator
+from commons.adapters.ChatGptAdapter import ChatGptAdapter
 from commons.adapters.GoogleSheetDatabaseAdapter import GoogleSheetDatabaseAdapter
+from configuration.AppConfig import AppConfig
+from configuration.AppConfig import Stage
 from dotenv import load_dotenv
 from fastapi import FastAPI
-import uvicorn
-from PaymentService.PaymentService import PaymentService
-from PaymentService.adapters.StripeAdapter import StripeAdapter
 from fastapi.middleware.cors import CORSMiddleware
-=======
+from pydantic import BaseModel
 from routers import StripeRouter, PaymentDatabaseRouter, PaymentServiceRouter
->>>>>>> c0b4eea (fix: create routers for FastAPI to clean up main file)
-
+import argparse, uvicorn
 
 tags_metadata = [
     {"name": "Payment Service", "description": "User accounts, billing, membership, UI"},
     {"name": "Payment Service: Stripe Adapter", "description": "Stripe object actions"},
     {"name": "Payment Service: Supabase", "description": "Database actions"},
 ]
-from pydantic import BaseModel
 
-app = FastAPI(openapi_tags=tags_metadata)
 class CreateCouponRequest(BaseModel):
     storeId: int
     gameId: str
@@ -61,6 +43,7 @@ class GetCouponRequest(BaseModel):
 class DestroyCouponRequest(BaseModel):
     couponId: str
 
+app = FastAPI(openapi_tags=tags_metadata)
 app.include_router(PaymentServiceRouter.router)
 app.include_router(PaymentDatabaseRouter.router)
 app.include_router(StripeRouter.router)
@@ -73,21 +56,6 @@ def generateLobbyQRCode(gameSessionId: str) -> str:
 def getQuestionAnswerSet():
     return questionService.getQuestionAnswerSet(10)
 
-<<<<<<< HEAD
-@app.post("/createNewUser", tags=["Payment Service"])
-def createNewUser(name: str, email: str):
-    """ Generate a new user account """
-    return paymentService.createAccount(name, email)
-
-@app.get("/listPaymentMethods", tags=["Payment Service: Stripe Adapter"])
-def listPaymentMethod(customerId: str):
-    """ Display all Payment Methods """
-    return stripeAdapter.listPaymentMethods(customerId)
-
-@app.post("/createIntent")
-def setUpPaymentIntent(paymentMethodID):
-    return PaymentService.createIntent(paymentMethodID)
-
 @app.post("/createCoupon")
 def createCoupon(createCouponRequest: CreateCouponRequest):
     return couponService.createCoupon(createCouponRequest.storeId, createCouponRequest.gameId)
@@ -96,15 +64,6 @@ def createCoupon(createCouponRequest: CreateCouponRequest):
 def assignCoupon(assignCouponRequest: AssignCouponRequest):
     return couponService.assignCoupon(assignCouponRequest.couponId, assignCouponRequest.winnerId)
 
-@app.delete("/deletePaymentMethod", tags=["Payment Service: Stripe Adapter"])
-def deletePaymentMethod(paymentMethodId):
-    """ Detaches a Payment Method from Customer; Not Retachable """
-    return stripeAdapter.detachPaymentMethod(paymentMethodId)
-
-@app.delete("/deleteAllPaymentMethods", tags=["Payment Service: Stripe Adapter"])
-def deleteAllPaymentMethods(customerId):
-    """ Detaches all Payment Methods attached to a Customer """
-    return stripeAdapter.detachAllPaymentMethods(customerId)
 @app.post("/createCoupon")
 def createCoupon(createCouponRequest: CreateCouponRequest):
     return couponService.createCoupon(createCouponRequest.storeId, createCouponRequest.gameId)
@@ -121,8 +80,6 @@ def getCoupon(getCouponRequest: GetCouponRequest):
 def destroyCoupon(destroyCouponRequest: DestroyCouponRequest):
     return couponService.destroyCoupon(destroyCouponRequest.couponId)
 
-=======
->>>>>>> c0b4eea (fix: create routers for FastAPI to clean up main file)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Configure environment for the application.')
     parser.add_argument('--env', type=str, choices=['dev', 'prod'], default='dev', help='Select the environment: dev or prod')
@@ -151,20 +108,16 @@ if __name__ == '__main__':
         allow_methods=[""],
         allow_headers=[""],
     )
+    
     load_dotenv()
+
     qrCodeGenerator = QRCodeGenerator(appConfig)
     lobbyService = LobbyService(qrCodeGenerator)
 
     chatGptAdapter = ChatGptAdapter()
     questionAnswerSetGenerator = QuestionAnswerSetGenerator(chatGptAdapter)
     questionService = QuestionService(chatGptAdapter, questionAnswerSetGenerator)
-<<<<<<< HEAD
 
-    paymentService = PaymentService()
-    stripeAdapter = StripeAdapter()
-=======
->>>>>>> c0b4eea (fix: create routers for FastAPI to clean up main file)
-    
     availableOffersAdapter = AvailableOffersAdapter()
     offerSelectionProcessor = OfferSelectionProcessor()
     couponIdGenerator = CouponIdGenerator()
@@ -174,4 +127,9 @@ if __name__ == '__main__':
     couponRedemptionAdapter = CouponRedemptionAdapter()
     customerMessagingProcessor = CustomerMessagingProcessor()
     couponService = CouponService(availableOffersAdapter, offerSelectionProcessor, couponIdGenerator, couponsDatabase, assignedCouponsDatabase, couponRedemptionAdapter, customerMessagingProcessor)
+
+    app.include_router(PaymentServiceRouter.router)
+    app.include_router(PaymentDatabaseRouter.router)
+    app.include_router(StripeRouter.router)
+    
     uvicorn.run(app, host="0.0.0.0", port=8000)
