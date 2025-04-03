25ea8b1200ea97732b6d6467dee092eef7f7440a
Daniel <dandcalvo@gmail.com>
2025-04-03 11:02:35 -0700

    feat: implementing new user handler

--- backend/routers/PaymentServiceRouter.py
+++ backend/routers/PaymentServiceRouter.py
@@ -9,6 +9,14 @@ router = APIRouter(
 
 paymentService = PaymentService()
 
+@router.post("/handleUserLogin")
+def handleUserLogin(auth0Id: str):
+    # check if user exists in client database
+    # if not , insert new client into client db
+
+    # check if user has stripe account
+    # if not, generate a new customer obj, then link customer id to client id
+
 @router.post("/createNewUser")
 def createNewUser(name: str, email: str):
     """ Generate a new user account """
