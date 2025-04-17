4e5a402e4e04c91a6071a5cd11a17f1b73b637d3
Daniel <dandcalvo@gmail.com>
2025-04-17 16:58:12 -0700

    feat: Subscription pricing table

--- frontend/QueuePlay/src/hooks/useHandleLogin.js
+++ frontend/QueuePlay/src/hooks/useHandleLogin.js
@@ -15,7 +15,6 @@ export const useHandleLogin = (user) => {
         axios
             .post(`http://127.0.0.1:8000/paymentdb/handleUserLogin`, payload)
             .then((response) => {
-                console.log(response)
                 setUserOutputData(response.data);
             })
             .catch((e) => {
